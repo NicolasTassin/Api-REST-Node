2d51c577e3fa00139bb76bd1d1f6112209265cad
NicolasTassin <nicolastassin598@gmail.com>
2020-09-13 11:49:36 +0200

    connection sql

--- app.js
+++ app.js
@@ -4,131 +4,149 @@ const morgan = require('morgan')
 const config = require('./config')
 const bodyParser = require('body-parser')
 const app = express()
+var mysql = require('mysql');
 
 
+var db = mysql.createConnection({
+    host: 'localhost',
+    database: 'api_node',
+    user: 'root',
+    password: ''
+});
 
-
-const members = [
-    {
-        id: 1,
-        name: 'Jonh'
-    },
-    {
-        id: 2,
-        name: 'Julie'
-    },
-    {
-        id: 3,
-        name: 'Jack'
+db.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
     }
-]
-let MembersRouter = express.Router()
-
-app.use(morgan('dev'))
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-MembersRouter.route('/:id')
-
-    .get((req, res) => {
-
-        let index = getIndex(req.params.id)
-
-        if (typeof(index) == 'string'){
-            res.json(error(index))
-        }else{
-            res.json(success(members[index]))
-        }
+    console.log('connected as id ' + db.threadId);
 
 
-        res.json(success(members[(req.params.id) - 1]))
-    })
+    db.query('SELECT * FROM members', (err, result) => {
+        if (err) console.log('error')
+        else {
+            console.log(result)
 
-    .put((req, res) => {
-        let index = getIndex(req.params.id)
+            let MembersRouter = express.Router()
 
-        if (typeof(index) == 'string'){
-            res.json(error(index))
-        }else{
-            
-            let same = false
-            for (let i = 0; i < members.length; i++) {
-                if (req.body.name == members[i].name && req.params.id !== members[i].id) {
-                same = true
-                break
-                }
-            }
+            app.use(morgan('dev'))
 
-            if (same){
-                res.json(error('same name'))
-            }else{
-                members[index].name = req.body.name
-                res.json(success(true))
-            }
-            
-        
-        }
+            app.use(bodyParser.urlencoded({ extended: false }))
+            app.use(bodyParser.json())
 
-    })
+            MembersRouter.route('/:id')
 
-    .delete((req, res) => {
-        let index = getIndex(req.params.id)
+                .get((req, res) => {
 
-        if (typeof(index) == 'string'){
-            res.json(error(index))
-        }else{
-            members.splice(index, 1)
-            res.json(success(members))
-        }
+                    let index = getIndex(req.params.id)
 
-    })
-
-    MembersRouter.route('/')
-
-        .get((req, res) => {
-            if (req.query.max != undefined && req.query.max > 0) {
-                res.json(success(members.slice(0, req.query.max)))
-            } else if (req.query.max != undefined) {
-                res.json(error('Wrong max value'))
-            } else {
-                res.json(success(members))
-            }
-        })
-
-        .post((req, res) => {
-            if (req.body.name) {
-
-                let sameName = false
-
-                for (let i = 0; i < members.length; i++) {
-                    if (members[i].name == req.body.name) {
-                        sameName = true
-                        break
+                    if (typeof (index) == 'string') {
+                        res.json(error(index))
+                    } else {
+                        res.json(success(members[index]))
                     }
-                }
-                if (sameName) {
-                    res.json(error('Name already taken'))
-                } else {
-                    let member = {
-                        id: createId(),
-                        name: req.body.name
+
+
+                    res.json(success(members[(req.params.id) - 1]))
+                })
+
+                .put((req, res) => {
+                    let index = getIndex(req.params.id)
+
+                    if (typeof (index) == 'string') {
+                        res.json(error(index))
+                    } else {
+
+                        let same = false
+                        for (let i = 0; i < members.length; i++) {
+                            if (req.body.name == members[i].name && req.params.id !== members[i].id) {
+                                same = true
+                                break
+                            }
+                        }
+
+                        if (same) {
+                            res.json(error('same name'))
+                        } else {
+                            members[index].name = req.body.name
+                            res.json(success(true))
+                        }
+
+
                     }
-                    members.push(member)
-                    res.json(success(member))
 
-                }
-            } else {
-                res.json(error('No name value'))
-            }
-        })
+                })
+
+                .delete((req, res) => {
+                    let index = getIndex(req.params.id)
+
+                    if (typeof (index) == 'string') {
+                        res.json(error(index))
+                    } else {
+                        members.splice(index, 1)
+                        res.json(success(members))
+                    }
+
+                })
+
+            MembersRouter.route('/')
+
+                .get((req, res) => {
+                    if (req.query.max != undefined && req.query.max > 0) {
+                        res.json(success(members.slice(0, req.query.max)))
+                    } else if (req.query.max != undefined) {
+                        res.json(error('Wrong max value'))
+                    } else {
+                        res.json(success(members))
+                    }
+                })
+
+                .post((req, res) => {
+                    if (req.body.name) {
+
+                        let sameName = false
+
+                        for (let i = 0; i < members.length; i++) {
+                            if (members[i].name == req.body.name) {
+                                sameName = true
+                                break
+                            }
+                        }
+                        if (sameName) {
+                            res.json(error('Name already taken'))
+                        } else {
+                            let member = {
+                                id: createId(),
+                                name: req.body.name
+                            }
+                            members.push(member)
+                            res.json(success(member))
+
+                        }
+                    } else {
+                        res.json(error('No name value'))
+                    }
+                })
 
 
-app.use(config.rootAPI+members, MembersRouter)
+            app.use(config.rootAPI + members, MembersRouter)
 
-app.listen(config.port, () => {
-    console.log('Started on port' + config.port)
-})
+            app.listen(config.port, () => {
+                console.log('Started on port' + config.port)
+            })
+
+
+
+
+        }
+    })
+});
+
+
+
+
+
+
 
 function getIndex(id) {
     for (let i = 0; i < members.length; i++) {
@@ -139,6 +157,6 @@ function getIndex(id) {
 }
 
 function createId() {
-    return lastMember = members[members.length-1].id +1
+    return lastMember = members[members.length - 1].id + 1
 }
 
